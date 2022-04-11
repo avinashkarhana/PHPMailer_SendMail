7238331f2ceab1bd469336cea5dce6907279d0c9
Avinash Karhana <avinashkarhana1@gmail.com>
2022-04-11 21:00:39 +0530

    Added attachment paths and sample Images

--- sample.php
+++ sample.php
@@ -23,10 +23,11 @@
     @$toName = $_POST['name'];
     @$message = $_POST['message'];
     @$subject = $_POST['subject'];
+    $attachmentsPaths = ['images/landscape.jpg', 'images/compass.jpg'];
 
     if(!empty($toEmail)){
         echo "Result: ";
-        if(sendMail("", "", $toEmail, $toName, $subject, $message, $message)) {
+        if(sendMail("", "", $toEmail, $toName, $subject, $message, $message, $attachmentsPaths)) {
             echo "Email message sent.";
         } else {
             echo "<br><hr>Failed to send email.";
